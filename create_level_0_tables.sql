86ab3058cec764b79410167eb720ebca55bee1db
Jefficus <jeff@smithicus.com>
2019-09-06 07:58:35 -0700

    More general cleanup and comment work. Also added some questions and supporting details.

--- create_level_0_tables.sql
+++ create_level_0_tables.sql
@@ -1,29 +1,32 @@
 CREATE TABLE IF NOT EXISTS level_0.sd_accel
 (
     iid bigint NOT NULL,   -- interact_id
-    ts timestamp NOT NULL, -- participant's local time
+    ts timestamp NOT NULL, -- participant's local time, to microsec 
     x double precision NOT NULL,
     y double precision NOT NULL,
     z double precision NOT NULL,
     PRIMARY KEY(iid,ts)
 );
+COMMENT ON TABLE level_0.sd_accel IS 'Contains all the valid accelerometer records extracted from the SenseDoc devices. Granularity = 20ms';
 
 CREATE TABLE IF NOT EXISTS level_0.sd_gps
 (
     iid bigint NOT NULL,  -- interact_id
-    ts timestamp NOT NULL, -- participant's local time
+    ts timestamp NOT NULL, -- participant's local time, to microsec
     lat double precision NOT NULL,
     lon double precision NOT NULL,
-    speed real NOT NULL,
-    course real NOT NULL,
+    speed real,
+    course real,
     mode char(1) DEFAULT '',
     fix char(1) DEFAULT '',
-    alt real NOT NULL,
+    alt real,
     mode1 char(1) DEFAULT '',
-    mode2 real NOT NULL,
-    sat_used real NOT NULL,
-    pdop real NOT NULL,
-    hdop real NOT NULL,
-    vdop real NOT NULL,
-    sat_in_view integer DEFAULT -1 
+    mode2 real,
+    sat_used real,
+    pdop real,
+    hdop real,
+    vdop real,
+    sat_in_view integer DEFAULT -1, 
+    PRIMARY KEY(iid,ts)
 );
+COMMENT ON TABLE level_0.sd_gps IS 'Contains all the valid GPS records extracted from the SenseDoc devices. Granularity = 1s';
