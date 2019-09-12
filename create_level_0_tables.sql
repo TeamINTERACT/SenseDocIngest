e5e2b2d38e447782867481ee21097e726ac924f9
Jefficus <jeff@smithicus.com>
2019-09-12 09:26:22 -0700

    Changed directory walking logic to account for the fact that some participant data is split across different directories when they use multiple SD devices. Also streamlined the number of columns in the gps table and improved the logging output.

--- create_level_0_tables.sql
+++ create_level_0_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS level_0.sd_accel
 (
     iid bigint NOT NULL,   -- interact_id
-    ts timestamp NOT NULL, -- participant's local time, to microsec 
+    ts timestamp with time zone NOT NULL, -- participant's UTC time, to millisec 
     x double precision NOT NULL,
     y double precision NOT NULL,
     z double precision NOT NULL,
@@ -12,20 +12,20 @@ COMMENT ON TABLE level_0.sd_accel IS 'Contains all the valid accelerometer recor
 CREATE TABLE IF NOT EXISTS level_0.sd_gps
 (
     iid bigint NOT NULL,  -- interact_id
-    ts timestamp NOT NULL, -- participant's local time, to microsec
+    ts timestamp with time zone NOT NULL, -- participant's UTC time, to millisec
     lat double precision NOT NULL,
     lon double precision NOT NULL,
-    speed real,
-    course real,
-    mode char(1) DEFAULT '',
-    fix char(1) DEFAULT '',
-    alt real,
-    mode1 char(1) DEFAULT '',
-    mode2 real,
-    sat_used real,
-    pdop real,
-    hdop real,
-    vdop real,
+    speed real DEFAULT 'NaN',
+    course real DEFAULT 'NaN',
+    -- mode char(1) DEFAULT '',
+    -- fix char(1) DEFAULT '',
+    alt real DEFAULT 'NaN',
+    -- mode1 char(1) DEFAULT '',
+    -- mode2 real DEFAULT 'NaN',
+    sat_used real DEFAULT 'NaN',
+    pdop real DEFAULT 'NaN',
+    hdop real DEFAULT 'NaN',
+    vdop real DEFAULT 'NaN',
     sat_in_view integer DEFAULT -1, 
     PRIMARY KEY(iid,ts)
 );
